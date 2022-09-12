3d12e1c7f1a27bb37cc114f337934f863123b2bf
Christopher <110996517+Nehmoi@users.noreply.github.com>
2022-09-12 03:16:50 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,16 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A c program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
