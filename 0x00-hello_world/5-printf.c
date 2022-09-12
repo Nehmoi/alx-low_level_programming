3e175943911bd6a5d68ea0136d1d373d958b1268
nehmoi <fathmick@gmail.com>
2022-09-11 19:23:31 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 5-printf.c
+#include <stdio.h>
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art.\n");
+	return (0);
+}
