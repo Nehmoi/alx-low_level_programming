e9ee8ebca933bcd07d91ceed11f1f847081f8525
nehmoi <fathmick@gmail.com>
2022-09-13 04:26:50 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers
+ *
+ * Return: Always (0) success
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		/*convert num to ASCII representation*/
+		putchar(num+ '0');
+		++num;
+	}
+	putchar('\n');
+
+	return (0);
+}
