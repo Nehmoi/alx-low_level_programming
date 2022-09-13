98cc31bd0610c4f603e3ccd60524f3623d203eef
nehmoi <fathmick@gmail.com>
2022-09-13 16:07:10 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: a function that prints the alphabet in lowercase
+ *
+ * Return: Always (0) success
+ */
+
+int main(void)
+{
+	print_alphabet();
+	
+	return (0);
+}
