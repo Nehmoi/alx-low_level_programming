f74f208e3bc2e154ae787ab6fefe8a48cd77dbfd
nehmoi <fathmick@gmail.com>
2022-09-13 05:06:21 -0700

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,16 +2,17 @@
 /**
  * main - Entry point
  * Description: prints all possible combinations of single-digit numbers.
- * Return: Always 0 (success)
+ * Return: Always (0) success
  */
 
 int main(void)
 {
 	int num = 0;
-	while (num <= 9)
 
+	while (num <= 9)
 	{
 		putchar(num + 48);
+
 		if (num != 9)
 		{
 			putchar(',');
