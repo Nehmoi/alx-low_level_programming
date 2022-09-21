e99d18678c7dc1da7515d2e58541bf421378b409
nehmoi <fathmick@gmail.com>
2022-09-20 18:22:56 -0700

    pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,16 +8,7 @@
  * Return: Always (0) success.
  */
 
-int main(void)
+void reset_to_98(int *n)
 {
-	int* pc,  n;
-
-	n = 402;
-	pc = &n;
-	n = 98;
-
-	printf("%d\n", n); /*output: 98*/
-	printf("%d\n", *pc); /*output: 98*/
-
-	return (0);
+	*n = 98;
 }
