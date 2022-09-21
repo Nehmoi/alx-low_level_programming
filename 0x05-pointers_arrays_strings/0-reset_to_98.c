a743027f53ce17e8a9f4ae4ecf3255528264df16
nehmoi <fathmick@gmail.com>
2022-09-20 18:25:47 -0700

    pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,8 @@
 
 /**
  * main - Entry point
- * Description: A function that takes a pointer to an int as
- * parameter and updates the value it points to
+ * Description: reset_to_98 - takes an integer pointer and updates
+ * the value to to 98
  * Return: Always (0) success.
  */
 
