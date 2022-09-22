b2486c38405d3e654385950bc40880712c2d88cf
nehmoi <fathmick@gmail.com>
2022-09-22 08:56:51 -0700

    updated

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,6 +4,8 @@
 /**
  * reset_to_98 - takes an integer as parameter and
  * updates the value it points to to 98.
+ * @n: an integer pointer that stores the address argument
+ * Return: Always (0) sucess
  */
 
 void reset_to_98(int *n)
