07ba5b23ba7084bce13cb08170e04b5252ca9e2b
nehmoi <fathmick@gmail.com>
2022-09-23 20:20:32 -0700

    updated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * _strcat - function appends the src string to the dest string, overwriting the terminating
- * null byte at the end of dest, and then adds a terminating null byte
+ * _strcat - function to apprend dest and src
  * @dest: destination string
  * @src: source string
  * Return: Always (0) success
