a8e911df9b0a48b62ebd0e7766bddec43dbca051
nehmoi <fathmick@gmail.com>
2022-09-29 07:26:44 -0700

    updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts - recursion function that prints a string.
@@ -8,14 +7,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s > 0)
+	if (*s == 0)
 	{
 		_puts_recursion(s + 1);
 		_putchar(*s);
 	}
-	_putchar('\n');
 	else
 	{
-		return (0);
+		_putchar('\n');
 	}
+	return (0);
 }
