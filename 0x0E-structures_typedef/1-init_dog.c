4480f56ea3beb68f98f01bd7ee2e2d78f3242008
nehmoi <fathmick@gmail.com>
2022-10-11 19:27:35 -0700

    File

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * struct dog - a function to initialize a variable type
+ *
+ * Return: Always 0.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    struct dog my_dog;
+
+    init_dog(&my_dog, "Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    return (0);
+}
