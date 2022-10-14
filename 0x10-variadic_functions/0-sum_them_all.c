a756a89f25add326885235800a1f6f18fa296e96
nehmoi <fathmick@gmail.com>
2022-10-14 04:20:22 -0700

    sum updated

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,26 +9,20 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-        /* create va_list for storing variables argument*/
+	va_list sumall;	/*create va_list for storing variables argument*/
+	unsigned int i;
+	int sum = 0;
 
-        va_list sumall;
-        unsigned int i;
-        int sum = 0;
+	if (n == 0)
+		return (0);
 
-        if (n == 0)
-                return (0);
+	va_start(sumall, n);	/*initialize va_list*/
 
-        /*initialize va_list for the numbers of argument*/
-        va_start(sumall, n);
-
-        /* loop through all arguments stored in the va_list*/
-
-        for (i = 0; i < n; i++)
-        {
-                sum += va_arg(sumall, int);
-        }
-
-        va_end(sumall);
-
-        return (sum);
+	/*loop through all argument stored in the va_list*/
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(sumall, int);
+	}
+	va_end(sumall);
+	return (sum);
 }
