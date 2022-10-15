b5cf49ebafd3875ad9a74a861d91df8d72eed465
nehmoi <fathmick@gmail.com>
2022-10-15 13:26:58 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * struct dag - a new type
+ * struct dog - a new data type
  *
  * Return nothing
  */
@@ -12,14 +12,10 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 int main(void)
 {
 	struct dog my_dog;
 
-	my_dog.name = "Poppy";
-	my_dog.age = 3.5;
-	my_dog.owner = "Bob";
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 	return(0);
 }
