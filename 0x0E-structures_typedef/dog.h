dcc894ff3f704d2ca9a7e8bd80fc5b31edfb1d3c
nehmoi <fathmick@gmail.com>
2022-10-16 16:15:28 -0700

    header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifdef DOG_H
+#define DOG_H
 
 /**
- * struct dog - a new data type
+ * struct dog - a dog's basic info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  *
- * Return nothing
+ * Description: nil
  */
 
 struct dog
@@ -14,10 +17,16 @@ struct dog
 	char *owner;
 };
 
-void main()
-{
-	struct dog my_dog = {"Poppy", 3.5, "Bob"};
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+/**
+ * dog_t - typedef for struct dog
+ */
 
-	return(0);
-}
+typedef struct dog dog_t;
+
+/*defining the struct data type and including various protype*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
