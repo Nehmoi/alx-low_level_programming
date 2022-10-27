0b82b716657543e1a70e7e397c7deca7b7423634
nehmoi <fathmick@gmail.com>
2022-10-26 17:39:06 -0700

    singly linked list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - a function that adds node at the beginning of the lis
+ *
+ * Return: address of the element or NULL if it failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	return(0);
+}
